cd1b8fc2d0ac4fa64edf3a39884deaf674d559eb
Michal Fularz <michal.fularz@gmail.com>
2014-12-12 02:22:55 +0100

    Some of older project copied into the new repository.

---
+++ CameraBase/CameraBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUTVision_CameraBase
+{
+    public abstract class CameraBase
+    {
+        protected int cameraNumber;
+        protected string cameraName;
+
+        protected int frameHeight;
+        protected int frameWidth;
+
+        protected string filenamePrefix;
+
+        protected void PrepareFilename()
+        {
+            this.filenamePrefix = "img_" + this.cameraName + "_";
+        }
+    }
+}
+
+// stare pomysły
+// jaki wspólne pola konfiguracyjne?
+// kolor / skala szarosci
+// ???
+
+// opcje:
+// wyswietlanie / zapisywanie / pomiar czasu
+
+// tryb pracy
+// asynchroniczny / synchroniczny
+
+// eventy
+// przesylanie informacji (message)
+// nastepna klatka (nextFrameEvent)
---
+++ ICamera/ICamera.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUTVision_ICamera
+{
+    public interface ICamera
+    {
+        void Open();
+        void Capture(int _imageNumber, bool _enableImageSave, bool _enableImageShow);
+        void Close();
+    }
+}
